306d61bd40e49000e58d5f8f70bfc38b91eb1c16
Diego Prieto <diegoprieto5@gmail.com>
2025-07-23 15:32:54 +0200

    add packages

--- install/xtras.sh
+++ install/xtras.sh
@@ -1,7 +1,8 @@
 yay -S --noconfirm --needed \
   telegram-desktop spotify \
   libreoffice obs-studio \
-  pinta xournalpp
+  pinta xournalpp \
+  globalprotect-openconnect-git vesktop
 
 # Copy over Omarchy applications
 source ~/.local/share/omarchy/bin/omarchy-sync-applications || true

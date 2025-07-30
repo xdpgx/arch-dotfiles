fca9fe3dc29b1650662c65a272a5721569fad7e4
Diego Prieto <diegoprieto5@gmail.com>
2025-07-30 12:49:02 +0200

    switch to ghostty

--- install/3-terminal.sh
+++ install/3-terminal.sh
@@ -3,4 +3,4 @@ yay -S --noconfirm --needed \
   fd eza fzf ripgrep zoxide bat \
   wl-clipboard fastfetch btop \
   man tldr less whois plocate \
-  alacritty bash-completion
+  ghostty alacritty bash-completion

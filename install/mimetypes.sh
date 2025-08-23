795fa57d9f75c467b93c43d8f4083fa5e0b0c31a
Diego Prieto <diegoprieto@strsistemas.com>
2025-08-23 15:49:12 +0200

    update default webbrowser

--- install/mimetypes.sh
+++ install/mimetypes.sh
@@ -12,9 +12,9 @@ xdg-mime default imv.desktop image/tiff
 xdg-mime default org.gnome.Evince.desktop application/pdf
 
 # Use Chromium as the default browser
-xdg-settings set default-web-browser chromium.desktop
-xdg-mime default chromium.desktop x-scheme-handler/http
-xdg-mime default chromium.desktop x-scheme-handler/https
+xdg-settings set default-web-browser firefox.desktop
+xdg-mime default firefox.desktop x-scheme-handler/http
+xdg-mime default firefox.desktop x-scheme-handler/https
 
 # Open video files with mpv
 xdg-mime default mpv.desktop video/mp4

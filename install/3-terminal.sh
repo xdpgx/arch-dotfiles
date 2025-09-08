428f5b9c86a84135d1c4df7d593576730cfab9bb
Diego Prieto <diegoprieto5@gmail.com>
2025-09-08 18:53:41 +0200

    remove alacritty

--- install/3-terminal.sh
+++ install/3-terminal.sh
@@ -3,4 +3,4 @@ yay -S --noconfirm --needed \
   fd eza fzf ripgrep zoxide bat \
   wl-clipboard fastfetch btop \
   man tldr less whois plocate \
-  ghostty alacritty bash-completion
+  ghostty bash-completion

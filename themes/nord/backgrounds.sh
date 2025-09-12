5f1248823a19700ea6b24a58378f003b8ba643e2
Diego Prieto <diegoprieto5@gmail.com>
2025-09-12 15:03:32 +0200

    update nord background

--- themes/nord/backgrounds.sh
+++ themes/nord/backgrounds.sh
@@ -1,2 +1,2 @@
 mkdir -p "$BACKGROUNDS_DIR/nord"
-download_background_image "https://github.com/basecamp/omakub/raw/refs/heads/master/themes/nord/background.png" "nord/1-nord.png"
+download_background_image "https://unsplash.com/photos/991czwDhaKo/download?ixid=M3wxMjA3fDB8MXxhbGx8fHx8fHx8fHwxNzU3NjgxNTk0fA&force=true" "nord/1-nord.png"
